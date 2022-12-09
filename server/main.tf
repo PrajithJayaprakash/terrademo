eab7def44572f7133e67a84c690c5b7881dce43b
PrajithJayaprakash <46163296+PrajithJayaprakash@users.noreply.github.com>
2022-12-09 20:13:40 +0530

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = var.vm_size
   admin_username        = var.admin_username
-  admin_password        = var. admin_password
+  admin_password        = var.admin_password
 
   os_disk {
     caching              = "ReadWrite"
